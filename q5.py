ee6e5010d1bd54c281d86e8e6ae899bb7cefe08d
preyasu-rakshit <preyasu.official@gmail.com>
2021-02-03 10:49:36 +0530

    q4

---
+++ q5.py
@@ -0,0 +1,15 @@
+# Solution by Anurag Mitra
+
+
+string= input("Enter string:")
+count1= 0
+count2= 0
+for i in string:
+      if(i.islower()):
+            count1=count1+1
+      elif(i.isupper()):
+            count2=count2+1
+print("The number of lowercase characters is:")
+print(count1)
+print("The number of uppercase characters is:")
+print(count2)
